80a976eb688fe692642e7db3c7054bcf8e165dd3
prasannaamar <adepuprasanna@gmail.com>
2026-05-22 22:26:19 +0530

    app created

---
+++ app.py
@@ -0,0 +1,29 @@
+import boto3
+
+# Create EC2 client
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+# Launch EC2 instance
+instances = ec2.create_instances(
+    ImageId='ami-0c02fb55956c7d316',  # Amazon Linux 2 AMI
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    KeyName='your-keypair-name',   # Replace with your key pair
+    SecurityGroupIds=['sg-xxxxxxxx'],
+    SubnetId='subnet-xxxxxxxx',
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'MyPythonEC2'
+                }
+            ]
+        }
+    ]
+)
+
+print("EC2 Instance Created!")
+print("Instance ID:", instances[0].id)
